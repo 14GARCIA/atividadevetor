9da8195741174b1ce91de0ec582d5d20459343f3
14GARCIA <162930592+14GARCIA@users.noreply.github.com>
2024-09-25 00:59:01 -0300

    Add files via upload

---
+++ HOSP/HOSP/Ficha.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOSP
+{
+    internal class Ficha
+    {
+        protected string nome;
+        protected string cpf;
+        protected string numeroTel;
+        protected string email;
+        protected string dataNasc;
+        protected int dia;
+        protected int mes;
+        protected int anp;
+        protected string cep;
+        protected string rua;
+        protected string bairro;
+        protected string cidade;
+        protected string tipoConsulta;
+        protected string sintomas;
+        protected string tempoSintomas;
+        protected string tempCorporal;
+        protected double altura;
+        protected double peso;
+        protected int numeroCasa;
+        protected int idade;
+        protected string complemento;
+        
+
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string NumeroTel { get; set; }
+        public string Email { get; set; }
+        public int Idade {  get; set; }
+
+        public string DataNasc {  get; set; } 
+        public int Dia { get; set; }
+        public int Mes { get; set; }
+        public int Ano { get; set; }
+        public double Peso {  get; set; }
+        public double Altura { get; set; }
+        public string Cep {  get; set; }
+        public string Rua {  get; set; }    
+        public string Bairro { get; set; }
+        public string Cidade {  get; set; }
+        public int NumeroCasa { get; set; }  
+        public string TipoConsulta {  get; set; }
+        public string Sintomas {  get; set; }   
+        public string TempoSintomas {  get; set; }  
+        public string TempCorporal { get; set;}
+
+        public string Complemento { get; set; } 
+
+
+
+
+
+
+    }
+}
---
+++ HOSP/HOSP/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOSP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ficha[] fichas = new Ficha[10];
+            int i = 0;
+
+            string ope;
+                Console.WriteLine("Olá!!\nBem vindo ao nosso sistema");
+
+            while (true)
+            {
+                Console.WriteLine("Escolha a opção  referente ao que voce deseja fazer no sistema: \n1- fazer uma ficha de um paciente \n2-Adicionar uma paciente na fila de espera \n3-Atender um paciente \n q-sair do sistema ");
+                ope = Console.ReadLine();
+                switch (ope) 
+                {
+                    case "1":
+                        {
+                            if ( i < 10) 
+                            {
+                                fichas[i] = new Ficha(); 
+                                Console.WriteLine("Digite o nome do paciente:");
+                                fichas[i].Nome = Console.ReadLine();
+                                do
+                                {
+                                    Console.WriteLine("Digite o CPF do paciente:");
+                                    fichas[i].Cpf = Console.ReadLine();
+                                    if (fichas[i].Cpf.Length != 11)
+                                    {
+                                        Console.WriteLine("CPF invalido");
+                                    }
+                                } while (fichas[i].Cpf.Length != 11);
+
+
+                                do {
+                                    Console.WriteLine("Digite o número de telefone do paciente:");
+                                    fichas[i].NumeroTel = Console.ReadLine();
+                                    fichas[i].NumeroTel = fichas[i].NumeroTel.Replace(" ", "");
+
+                                    if (fichas[i].NumeroTel.Length != 11 || !long.TryParse(fichas[i].NumeroTel, out _))
+                                    {
+                                        Console.WriteLine("Telefone invalido, o numero de telefone deve conter 11 numeros contando com o dd");
+                                    }
+                                }while (fichas[i].NumeroTel.Length != 11 || !long.TryParse(fichas[i].NumeroTel, out _));
+
+
+                                Console.WriteLine("Digite o email:");
+                                fichas[i].Email = Console.ReadLine();
+
+                                Console.WriteLine("Digite a data de nascimento do paciente:");
+                                Console.WriteLine("dia:");
+                                fichas[i].Dia=int.Parse(Console.ReadLine());
+                                Console.WriteLine("Mes:");
+                                fichas[i].Mes=int.Parse(Console.ReadLine());
+                                Console.WriteLine("Ano:");
+                                fichas[i].Ano=int.Parse(Console.ReadLine());
+                                fichas[i].DataNasc = fichas[i].Dia + "/" + fichas[i].Mes + "/" + fichas[i].Ano;
+
+                                Console.WriteLine("Informe a idade:");
+                                fichas[i].Idade = int.Parse(Console.ReadLine());
+
+                                Console.WriteLine("Digite a altura do paciente:");
+                                fichas[i].Altura = double.Parse(Console.ReadLine());
+
+                                Console.WriteLine("Informe o peso do paciente:");
+                                fichas[i].Peso = double.Parse(Console.ReadLine());
+
+                                Console.WriteLine("Digite o CEP:");
+                                fichas[i].Cep = Console.ReadLine();
+
+                                Console.WriteLine("Cadastre a rua do paciente:");
+                                fichas[i].Rua = Console.ReadLine();
+
+                                Console.WriteLine("Digite o bairro:");
+                                fichas[i].Bairro = Console.ReadLine();
+
+                                Console.WriteLine("Digite o nome da cidade:");
+                                fichas[i].Cidade = Console.ReadLine();
+
+                                Console.WriteLine("Número da casa:");
+                                fichas[i].NumeroCasa = int.Parse(Console.ReadLine());
+
+                                Console.WriteLine("Se houver, digite o complemento do endereço:");
+                                fichas[i].Complemento = Console.ReadLine();
+
+                               
+                                int tipo;
+                                do
+                                {
+                                    Console.WriteLine("Selecione o tipo de consulta: \n1 - Particular\n2 - Convênio");
+                                    tipo = int.Parse(Console.ReadLine());
+
+                                    switch (tipo)
+                                    {
+                                        case 1:
+                                            fichas[i].TipoConsulta = "Consulta Particular";
+                                            break;
+                                        case 2:
+                                            fichas[i].TipoConsulta = "Consulta pelo Convênio";
+                                            break;
+                                        default:
+                                            Console.WriteLine("Digite uma opção válida.");
+                                            break;
+                                    }
+                                } while (tipo != 1 && tipo != 2);
+                                Console.WriteLine("Quais sintomas o paciente esta sentindo:");
+                                fichas[i].Sintomas = Console.ReadLine();
+
+                                Console.WriteLine("ha quanto tempo o paciente esses sentindo esses sintomas:");
+                                fichas[i].TempoSintomas = Console.ReadLine();
+
+                                Console.WriteLine("Qual a temperatura do corpo do paciente: ");
+                                fichas[i].TempCorporal = Console.ReadLine();
+
+
+                                i++; 
+                                Console.WriteLine("Ficha cadastrada com sucesso!");
+      
+                            }
+                            else
+                            {
+                                Console.WriteLine("Número máximo de fichas atingido.");
+                            }
+                            int visu;
+                            do
+                            {
+
+                                Console.WriteLine("voce deseja visualizar todas as fichas cadastradas?\n1-sim\n2-nao");
+                                visu = int.Parse(Console.ReadLine());
+
+                                switch (visu)
+                                {
+                                    case 1:
+                                        {
+                                            for (int j = 0; j <= i; j++)
+                                            {
+                                                Console.WriteLine($"\n Dados do Paciente {j + 1} ");
+                                                Console.WriteLine($"Nome: {fichas[j].Nome}");
+                                                Console.WriteLine($"CPF: {fichas[j].Cpf}");
+                                                Console.WriteLine($"Número de Telefone: {fichas[j].NumeroTel}");
+                                                Console.WriteLine($"Email: {fichas[j].Email}");
+                                                Console.WriteLine($"Data de Nascimento: {fichas[j].DataNasc}");
+                                                Console.WriteLine($"Idade: {fichas[j].Idade}");
+                                                Console.WriteLine($"Altura: {fichas[j].Altura}");
+                                                Console.WriteLine($"Peso: {fichas[j].Peso}");
+                                                Console.WriteLine($"CEP: {fichas[j].Cep}");
+                                                Console.WriteLine($"Rua: {fichas[j].Rua}");
+                                                Console.WriteLine($"Bairro: {fichas[j].Bairro}");
+                                                Console.WriteLine($"Cidade: {fichas[j].Cidade}");
+                                                Console.WriteLine($"Número da Casa: {fichas[j].NumeroCasa}");
+                                                Console.WriteLine($"Complemento: {fichas[j].Complemento}");
+                                                Console.WriteLine($"Tipo de Consulta: {fichas[j].TipoConsulta}");
+                                                Console.WriteLine($"Sintomas: {fichas[j].Sintomas}");
+                                                Console.WriteLine($"Tempo dos Sintomas: {fichas[j].TempoSintomas}");
+                                                Console.WriteLine($"Temperatura Corporal: {fichas[j].TempCorporal}");
+                                            }
+                                            break;
+                                        }
+                                    case 2:
+                                        {
+                                            Console.WriteLine("Obrigado por cdastrar o paciente");
+                                            break;
+                                        }
+                                    default:
+                                        {
+                                            Console.WriteLine("Digite uma opcao valida");
+                                            break;
+                                        }
+                                }
+                            } while ( visu  != 1 && visu != 2);
+                            break;
+                            }
+                    case "2":
+                        {
+
+                            break;
+                        }
+                    case "3": 
+                        {
+                            break;
+                        }
+                    case "q": 
+                        {
+                            Console.WriteLine("voce quis sair do sistema!!");
+                            return;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Opção inválida, tente novamente.");
+                            break;
+                        }
+
+                        }
+
+                }
+
+            Console.ReadKey();
+        }
+
+            
+        
+    }
+}
